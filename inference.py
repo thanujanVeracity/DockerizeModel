b79430de9baecd66029d3afb907f2caf4c6bfa5c
Tharmapalan Thanujan <thajan@pop-os.localdomain>
2023-01-10 14:11:00 +0530

    feat: dockerized inference

---
+++ inference.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#! /usr/bin/python3
+# train.py
+# Author Thanujan T
+
+import torch
+import matplotlib.pyplot as plt
+
+def inference():
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    img = 'https://i.ytimg.com/vi/q71MCWAEfL8/maxresdefault.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+    results = model(img)
+    fig, ax = plt.subplots(figsize=(16, 12))
+    ax.imshow(results.render()[0])
+    plt.show()
+    
+if __name__ == '__main__':
+    inference()
+
+
+
